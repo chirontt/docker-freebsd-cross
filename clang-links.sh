9701f051362471fd77a5545f4fcdea8069efcf00
dsk799 <96516959+dsk799@users.noreply.github.com>
2023-09-29 12:43:21 +0200

    Update clang-links.sh
    Signed-off-by: dsk799 <96516959+dsk799@users.noreply.github.com>

--- clang-links.sh
+++ clang-links.sh
@@ -7,9 +7,9 @@ triple=${2:-x86_64-unknown-freebsd13}
 # https://github.com/rust-lang/rust/blob/master/src/ci/docker/scripts/freebsd-toolchain.sh
 for tool in clang clang++; do
   tool_path=/usr/local/bin/${triple}-${tool}
-  cat > "$tool_path" <<EOF
+  cat > "${tool_path}" <<EOF
 #!/bin/sh
-exec $tool --sysroot=$sysroot "\$@" --target=$triple
+exec ${tool} --sysroot=${sysroot} "\$@" --target=${triple}
 EOF
-  chmod +x "$tool_path"
+  chmod +x "${tool_path}"
 done
